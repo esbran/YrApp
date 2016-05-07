a2cdbf4ece5663602fc0edf8038aab25f836c38a
Espen Brandt-Kjelsen <esbran@microsoft.com>
2016-05-07 14:16:36 +0200

    Add project files.

---
+++ YrApp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace YrApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get weather";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 65);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(232, 139);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 534);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ YrApp/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using RestSharp;
+
+namespace YrApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string url = "http://www.yr.no/place/Norway/Oslo/Oslo/Oslo/forecast.xml";
+        WeatherData weather; 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            richTextBox1.Text = GetWeatherXml();
+            weather = GetWeatherData();
+            label1.Text = string.Format("Temp: {0}", weather.WeatherItems[0].Temp);
+            label2.Text = string.Format("Wind: {0}", weather.WeatherItems[0].WindSpeedName);
+            label3.Text = string.Format("Precipitation: {0}", weather.WeatherItems[0].PrecipitationValue);
+            label4.Text = string.Format("Time: {0}", weather.WeatherItems[0].TimeFrom);
+        }
+        public string GetWeatherXml()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            return client.Execute(request).Content;
+        }
+
+        public WeatherData GetWeatherData()
+        {
+            WeatherData Data = new WeatherData() { WeatherItems = new List<WeatherItem>() };
+
+            StringBuilder sb = new StringBuilder("");
+
+            System.Xml.XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(GetWeatherXml());
+
+            System.Xml.XmlNode sun = doc.SelectSingleNode("/weatherdata/sun");
+            System.Xml.XmlNode location = doc.SelectSingleNode("/weatherdata/location");
+            System.Xml.XmlNodeList nodelist = doc.SelectNodes("/weatherdata/forecast/tabular/time");
+
+            Data.SunRise = sun.Attributes["rise"].Value;
+            Data.SunSet = sun.Attributes["set"].Value;
+            Data.IsFilled = true;
+
+            Data.LocationName = location["name"].Value;
+            Data.LocationType = location["type"].Value;
+            Data.LocationCountry = location["country"].Value;
+
+            Data.LocationLatitude = location["location"].Attributes["latitude"].Value;
+            Data.LocationLongitude = location["location"].Attributes["longitude"].Value;
+
+            for (int i = 0; i < 7; i++)
+            {
+                WeatherItem item = new WeatherItem();
+
+                item.Period = nodelist[i].Attributes["period"].Value;
+                item.TimeFrom = DateTime.Parse(nodelist[i].Attributes["from"].Value).ToString("yyyy-MM-dd HH:mm");
+                item.TimeTo = DateTime.Parse(nodelist[i].Attributes["to"].Value).ToString("yyyy-MM-dd HH:mm");
+                item.SymbolNumber = nodelist[i]["symbol"].Attributes["number"].Value.PadLeft(2, '0');
+                item.SymbolName = nodelist[i]["symbol"].Attributes["name"].Value;
+                item.Temp = nodelist[i]["temperature"].Attributes["value"].Value + "C";
+                item.PrecipitationValue = nodelist[i]["precipitation"].Attributes["value"].Value;
+                item.WindSpeedMPS = nodelist[i]["windSpeed"].Attributes["mps"].Value;
+                item.WindSpeedName = nodelist[i]["windSpeed"].Attributes["name"].Value;
+                item.WindDirectionCode = nodelist[i]["windDirection"].Attributes["code"].Value;
+                item.WindDirectionDeg = nodelist[i]["windDirection"].Attributes["deg"].Value;
+                item.WindDirectionName = nodelist[i]["windDirection"].Attributes["name"].Value;
+
+                item.IsFilled = true;
+
+                Data.WeatherItems.Add(item);
+            }
+
+            return Data;
+        }
+
+
+        public class WeatherItem
+        {
+            public string TimeFrom { get; set; }
+            public string TimeTo { get; set; }
+
+            public string Period { get; set; }
+
+            public string SymbolNumber { get; set; }
+            public string SymbolName { get; set; }
+            public string SymbolVar { get; set; }
+
+            public string PrecipitationValue { get; set; }
+
+            public string WindDirectionDeg { get; set; }
+            public string WindDirectionCode { get; set; }
+            public string WindDirectionName { get; set; }
+            public string WindSpeedMPS { get; set; }
+            public string WindSpeedName { get; set; }
+
+            public string Temp { get; set; }
+            public string Pressure { get; set; }
+
+            public bool IsFilled { get; set; }
+
+        }
+
+        public class WeatherData
+        {
+            public string LocationName { get; set; }
+            public string LocationType { get; set; }
+            public string LocationCountry { get; set; }
+            public string LocationLatitude { get; set; }
+            public string LocationLongitude { get; set; }
+
+            public string SunRise { get; set; }
+            public string SunSet { get; set; }
+
+            public List<WeatherItem> WeatherItems { get; set; }
+
+            public bool IsFilled { get; set; }
+
+            public WeatherData()
+            {
+                WeatherItems = new List<WeatherItem>();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
